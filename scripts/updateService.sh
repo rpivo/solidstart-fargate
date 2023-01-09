7a18123c6a06596e8f68b236b94a90aeb107c5a2
Ryan <ryanpivovar@gmail.com>
2023-01-09 08:46:12 -0500

    add region arg to updateService script

--- scripts/updateService.sh
+++ scripts/updateService.sh
@@ -1,6 +1,7 @@
 source ./scripts/env.sh
 
 aws ecs update-service \
+  --region $AWS_REGION \
   --profile $AWS_PROFILE \
   --cluster SolidStartEcsCluster \
   --service SolidStartApplicationLoadBalancedFargateService \
